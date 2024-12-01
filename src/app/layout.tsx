8804ca366ecf1115d35eb5306d3b94140fae625e
ducchien24 <chien24022001a@gmail.com>
2024-12-02 01:57:30 +0700

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+
+import ThemeRegistry from '@/components/theme-registry/theme.registry';
+import NextAuthWrapper from '@/lib/next.auth.wrapper';
+import { TrackContextProvider } from '@/lib/track.wrapper';
+import { ToastProvider } from '@/utils/toast';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">  
+      <body>
+        <ThemeRegistry>
+          <NextAuthWrapper>
+            <ToastProvider>
+              <TrackContextProvider>
+                {children}
+              </TrackContextProvider>
+            </ToastProvider>
+          </NextAuthWrapper>
+        </ThemeRegistry>
+      </body>
+    </html>
+  );
+}
